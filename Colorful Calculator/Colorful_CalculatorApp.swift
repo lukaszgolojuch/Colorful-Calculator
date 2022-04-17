faa9ea03959e0045e394ba8f8dedcb0c87c172cd
lukaszgolojuch <lukasz.golojuch@gmail.com>
2022-04-17 20:21:55 +0200

    Initial Commit

---
+++ Colorful Calculator/Colorful_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Colorful_CalculatorApp.swift
+//  Colorful Calculator
+//
+//  Created by Łukasz Gołojuch on 4/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Colorful_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Colorful Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Colorful Calculator
+//
+//  Created by Łukasz Gołojuch on 4/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
