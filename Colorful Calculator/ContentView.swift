cae6bbc2b73c2a4486098971170317a1562021fc
lukaszgolojuch <lukasz.golojuch@gmail.com>
2022-04-18 22:18:56 +0200

    Basic SwiftUI view for calculator - no options added

--- Colorful Calculator/ContentView.swift
+++ Colorful Calculator/ContentView.swift
@@ -7,10 +7,94 @@
 
 import SwiftUI
 
+enum Buttons: String {
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case zero = "0"
+    case add = "+"
+    case substract = "-"
+    case divide = "/"
+    case multiply = "x"
+    case equal = "="
+    case clear = "AC"
+    case decimal = "."
+    case percent = "%"
+    case negative = "-/+"
+    
+    var colorOfButton: Color{
+        switch self{
+        case .add, .substract, .divide, .multiply, .equal:
+            return .orange
+        case .clear, .negative, .percent:
+            return Color(.lightGray)
+        default:
+            return .cyan
+        }
+    }
+}
+
 struct ContentView: View {
+    
+    let buttons: [[Buttons]] = [
+        [.clear, .negative, .percent, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .substract],
+        [.one, .two, .three, .add],
+        [.zero, .decimal, .equal]
+    ]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack{
+                Spacer()
+                HStack{
+                    Spacer()
+                    Text("0")
+                        .bold()
+                        .font(.system(size: 52))
+                        .foregroundColor(.white)
+                }
+                .padding()
+                
+                ForEach(buttons, id: \.self) { row in
+                    HStack{
+                        ForEach(row, id: \.self) {object in
+                            Button(action: {
+                                print("Done")
+                            }) {
+                                Text(object.rawValue)
+                                    .font(.system(size: 32))
+                                    .frame(
+                                        width: getWidth(selectedButton: object),
+                                        height: getHeight(selectedButton: object))
+                                    .background(object.colorOfButton)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(35)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func getWidth(selectedButton: Buttons) -> CGFloat {
+        if selectedButton == Buttons.zero {
+            return (UIScreen.main.bounds.width - (5*12)) / 2
+        }
+        return (UIScreen.main.bounds.width - (5*12)) / 4
+    }
+    
+    func getHeight(selectedButton: Buttons) -> CGFloat {
+        return (UIScreen.main.bounds.width - (5*12)) / 4
     }
 }
 
